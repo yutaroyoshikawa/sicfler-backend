7e8ea797447f69d506c16b89ad3f29d3df3e14f7
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-02-06 09:52:33 +0900

    apolloインストール

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("hello!!");
+import { ApolloServer } from "apollo-server-lambda";
+import { typeDefs, resolvers } from "./src/main";
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const handler = server.createHandler();
+
+export default handler;
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { gql, IResolvers } from "apollo-server-lambda";
+
+export const typeDefs = gql``;
+
+export const resolvers: IResolvers = {};
