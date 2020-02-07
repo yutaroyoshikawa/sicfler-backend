fb4ca2ae81ace5add625baa438db76e0c0a818f4
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-02-08 01:00:53 +0900

    graphql環境整備

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
-import { gql, IResolvers } from "apollo-server-lambda";
+import { IResolvers } from "apollo-server-lambda";
+import fs from "fs";
+import path from "path";
+import Mutations from "./mutations";
+import Queries from "./queries";
 
-export const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+export const typeDefs = fs
+  .readFileSync(path.join(__dirname, "./graphql/schema.graphql"))
+  .toString();
 
-export const resolvers: IResolvers = {
-  Query: {
-    hello: () => "Hello world!"
-  }
-};
+export const resolvers: IResolvers = { Queries, Mutations };
---
+++ src/queries.ts
@@ -0,0 +1,3 @@
+const Queries = {};
+
+export default Queries;
