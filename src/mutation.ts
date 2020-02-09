727b2ab8dc1cdfa9792ddc312fe13e7413738d97
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-02-09 14:51:10 +0900

    ダミーデータ作成

--- src/mutation.ts
+++ src/mutation.ts
@@ -14,6 +14,90 @@ const Mutation: MutationResolvers = {
       password: "",
       permission: "user"
     };
+  },
+  async addOrner(_parent, args) {
+    return {
+      id: "",
+      email: args.email,
+      name: args.name,
+      discription: args.discription,
+      icon: args.icon,
+      images: [],
+      address: args.address,
+      location: {
+        xIndex: args.location?.xIndex,
+        yIndex: args.location?.yIndex
+      }
+    };
+  },
+  async updateOrner(_parent, args) {
+    return {
+      id: "",
+      email: args.email ? args.email : "",
+      name: args.name ? args.name : "",
+      discription: args.discription,
+      icon: args.icon,
+      images: args.images ? args.images : [],
+      address: args.address,
+      location: {
+        xIndex: args.location?.xIndex,
+        yIndex: args.location?.yIndex
+      }
+    };
+  },
+  async addPost(_parent, args) {
+    return {
+      id: "",
+      name: args.name,
+      start: args.start,
+      finish: args.finish,
+      discription: args.discription,
+      sumbnail: args.discription,
+      images: [],
+      visitors: [],
+      orner: {
+        id: args.ornerId,
+        email: "",
+        name: "",
+        discription: "",
+        icon: "",
+        images: [],
+        address: "",
+        location: {
+          xIndex: 0,
+          yIndex: 0
+        }
+      },
+      address: args.address,
+      location: {
+        xIndex: args.location?.xIndex,
+        yIndex: args.location?.yIndex
+      },
+      target: {
+        ageGroup: 10,
+        gender: 1
+      }
+    };
+  },
+  async deletePost(_parent, args) {
+    return {
+      id: args.id,
+      name: "",
+      start: new Date(),
+      finish: new Date(),
+      images: [],
+      visitors: [],
+      orner: {
+        id: "",
+        email: "",
+        name: "",
+        images: []
+      },
+      target: {
+        ageGroup: 20,
+        gender: 0
+      }
+    };
   }
 };
 
--- src/query.ts
+++ src/query.ts
@@ -16,6 +16,68 @@ const Query: QueryResolvers = {
         permission: "hoge"
       }
     ];
+  },
+  async orner(_parent, args) {
+    return {
+      id: args.id,
+      email: "",
+      name: "",
+      discription: "",
+      images: []
+    };
+  },
+  async orners() {
+    return [
+      {
+        id: "hoge",
+        email: "",
+        name: "",
+        discription: "",
+        images: []
+      }
+    ];
+  },
+  async post(_parent, args) {
+    return {
+      id: args.id,
+      name: "",
+      start: new Date(),
+      finish: new Date(),
+      images: [],
+      visitors: [],
+      orner: {
+        id: "",
+        email: "",
+        name: "",
+        images: []
+      },
+      target: {
+        ageGroup: 20,
+        gender: 0
+      }
+    };
+  },
+  async posts() {
+    return [
+      {
+        id: "",
+        name: "",
+        start: new Date(),
+        finish: new Date(),
+        images: [],
+        visitors: [],
+        orner: {
+          id: "",
+          email: "",
+          name: "",
+          images: []
+        },
+        target: {
+          ageGroup: 20,
+          gender: 0
+        }
+      }
+    ];
   }
 };
 
