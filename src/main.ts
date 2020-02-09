cdb264b69a9c50f3c4fcb84a77a849e1d4f20057
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-02-09 12:19:39 +0900

    ファイル名変更

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { Config } from "apollo-server-lambda";
-import Mutation from "./mutations";
-import Query from "./queries";
+import Mutation from "./mutation";
+import Query from "./query";
 import { Resolvers } from "./generated/graphql";
 import typeDefs from "./graphql/schema.graphql";
 
