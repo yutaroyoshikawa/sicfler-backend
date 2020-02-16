db9fd48c48746ce31ed176425ab25bc8fc1b188f
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-02-16 13:27:10 +0900

    graphqlスキーマ修正

--- src/cognito.ts
+++ src/cognito.ts
@@ -1,16 +1,20 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { CognitoIdentityServiceProvider, config } from "aws-sdk";
+import {
+  CognitoIdentityServiceProvider,
+  CognitoIdentity,
+  config
+} from "aws-sdk";
 
 config.update({ region: process.env.AWS_REGION! });
 
-const cognito = new CognitoIdentityServiceProvider({
+export const cognitoAdmin = new CognitoIdentityServiceProvider({
   apiVersion: "2016-04-18"
 });
+
+export const cognito = new CognitoIdentity({ apiVersion: "2014-06-30" });
 
 export enum Permissions {
   Admin = "ADMIN",
   Orner = "ORNER",
   User = "USER"
 }
-
-export default cognito;
