c32ef7b0f4366a245593741d5230345cb496957e
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-02-16 16:08:44 +0900

    ornerの通信処理追加

--- src/mutation.ts
+++ src/mutation.ts
@@ -1,13 +1,25 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { ApolloError } from "apollo-server-lambda";
-import { MutationResolvers } from "./generated/graphql";
-// import DB, { Tables } from "./dynamoDB";
+import { MutationResolvers, Roles } from "./generated/graphql";
 import { cognitoAdmin } from "./cognito";
+import DB, { Tables } from "./dynamoDB";
+
+const UserPoolId = process.env.COGNITO_POOL_ID!;
 
 const Mutation: MutationResolvers = {
   async addUser(_parent, args) {
     const response = await cognitoAdmin
-      .adminCreateUser()
+      .adminCreateUser({
+        UserPoolId,
+        Username: args.email,
+        TemporaryPassword: args.password,
+        UserAttributes: [
+          {
+            Name: "role",
+            Value: Roles.User
+          }
+        ]
+      })
       .promise()
       .catch(err => {
         throw new ApolloError(err);
@@ -21,7 +33,10 @@ const Mutation: MutationResolvers = {
   },
   async deleteUser(_parent, args) {
     await cognitoAdmin
-      .adminDeleteUser()
+      .adminDeleteUser({
+        UserPoolId,
+        Username: args.id
+      })
       .promise()
       .catch(err => {
         throw new ApolloError(err);
@@ -34,34 +49,106 @@ const Mutation: MutationResolvers = {
     };
   },
   async addOrner(_parent, args) {
-    return {
-      id: "",
+    await cognitoAdmin
+      .adminUpdateUserAttributes({
+        UserPoolId,
+        Username: args.id,
+        UserAttributes: [
+          {
+            Name: "role",
+            Value: Roles.Orner
+          }
+        ]
+      })
+      .promise()
+      .catch(err => {
+        throw new ApolloError(err);
+      });
+
+    const orner = {
+      id: args.id,
       email: args.email,
       name: args.name,
       discription: args.discription,
       icon: args.icon,
-      images: [],
+      images: args.images,
       address: args.address,
       location: {
         xIndex: args.location?.xIndex,
         yIndex: args.location?.yIndex
       }
     };
+
+    await DB.put({
+      TableName: Tables.OrnersTable,
+      Item: orner
+    })
+      .promise()
+      .catch(err => {
+        throw new ApolloError(err);
+      });
+
+    return orner;
   },
   async updateOrner(_parent, args) {
-    return {
-      id: "",
-      email: args.email ? args.email : "",
-      name: args.name ? args.name : "",
-      discription: args.discription,
-      icon: args.icon,
-      images: args.images ? args.images : [],
-      address: args.address,
+    const orner = {
+      id: args.id!,
+      email: args.email!,
+      name: args.name!,
+      discription: args.discription!,
+      icon: args.icon!,
+      images: args.images!,
+      address: args.address!,
       location: {
         xIndex: args.location?.xIndex,
         yIndex: args.location?.yIndex
       }
     };
+
+    await DB.update({
+      TableName: Tables.OrnersTable,
+      Key: {
+        Name: "id",
+        Value: args.id
+      },
+      AttributeUpdates: {
+        email: {
+          Value: orner.email,
+          Action: "PUT"
+        },
+        name: {
+          Value: orner.name,
+          Action: "PUT"
+        },
+        discription: {
+          Value: orner.discription,
+          Action: "PUT"
+        },
+        icon: {
+          Value: orner.icon,
+          Action: "PUT"
+        },
+        images: {
+          Value: orner.icon,
+          Action: "PUT"
+        },
+        address: {
+          Value: orner.address,
+          Action: "PUT"
+        },
+        location: {
+          Value: orner.location,
+          Action: "PUT"
+        }
+      },
+      ReturnValues: "ALL_NEW"
+    })
+      .promise()
+      .catch(err => {
+        throw new ApolloError(err);
+      });
+
+    return orner;
   },
   async addPost(_parent, args) {
     return {
