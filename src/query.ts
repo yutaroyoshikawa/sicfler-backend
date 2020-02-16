5177bb8fe2a8ed3b67d478c126ee75effe6a24d1
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-02-16 14:53:56 +0900

    データベース から取得するように修正

--- src/query.ts
+++ src/query.ts
@@ -1,41 +1,98 @@
-import { QueryResolvers } from "./generated/graphql";
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { ApolloError } from "apollo-server-lambda";
+import { QueryResolvers, Roles } from "./generated/graphql";
+import { cognito, cognitoAdmin } from "./cognito";
+import db, { Tables } from "./dynamoDB";
 
 const Query: QueryResolvers = {
   async user(_parent, args) {
+    const res = await cognitoAdmin
+      .adminGetUser({
+        UserPoolId: args.id,
+        Username: args.id
+      })
+      .promise()
+      .catch(err => {
+        throw new ApolloError(err);
+      });
+
     return {
-      email: args.email,
-      password: "hoge",
-      permission: "hoge"
+      id: res.Username,
+      creationDate: res.UserCreateDate,
+      lastModified: res.UserLastModifiedDate,
+      role: res.UserAttributes!.find(attribute => attribute.Name === "role")
+        ?.Value as Roles
     };
   },
   async users(_parent, args, context) {
-    return [
-      {
-        email: context.userName,
-        password: "hoge",
-        permission: "hoge"
-      }
-    ];
+    const res = await cognito
+      .listIdentities({
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        IdentityPoolId: process.env.COGNITO_POOL_ID!,
+        MaxResults: 100
+      })
+      .promise()
+      .catch(err => {
+        throw new ApolloError(err);
+      });
+
+    const users = res.Identities
+      ? res.Identities.map(idetity => ({
+          id: idetity.IdentityId!,
+          creationDate: idetity.CreationDate!,
+          lastModified: idetity.LastModifiedDate!
+        }))
+      : [];
+
+    return users;
   },
   async orner(_parent, args) {
+    const response = await db
+      .get({
+        TableName: Tables.OrnersTable,
+        Key: {
+          Name: "id",
+          Value: args.id
+        }
+      })
+      .promise()
+      .catch(err => {
+        throw new ApolloError(err);
+      });
+
     return {
-      id: args.id,
-      email: "",
-      name: "",
-      discription: "",
-      images: []
+      id: response.Item!.id,
+      email: response.Item!.email,
+      name: response.Item!.name,
+      discription: response.Item!.discription,
+      images: response.Item!.images
     };
   },
   async orners() {
-    return [
-      {
-        id: "hoge",
-        email: "",
-        name: "",
-        discription: "",
-        images: []
-      }
-    ];
+    const response = await db
+      .scan({
+        TableName: Tables.OrnersTable
+      })
+      .promise()
+      .catch(err => {
+        throw new ApolloError(err);
+      });
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const orners = response.Items
+      ? response.Items?.map(orner => ({
+          id: orner.id,
+          email: orner.email,
+          name: orner.name,
+          discription: orner.discription,
+          icon: orner.icon,
+          images: orner.images,
+          address: orner.address,
+          location: orner.location
+        }))
+      : [];
+
+    return orners;
   },
   async post(_parent, args) {
     return {
