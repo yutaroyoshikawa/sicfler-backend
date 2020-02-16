72a3fa6a3994c29b0bbd7800c2153807c24f0fd6
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-02-16 11:20:52 +0900

    cognito admin作成

--- src/cognito.ts
+++ src/cognito.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { CognitoIdentity, config } from "aws-sdk";
+import { CognitoIdentityServiceProvider, config } from "aws-sdk";
 
 config.update({ region: process.env.AWS_REGION! });
 
-const cognito = new CognitoIdentity();
+const cognito = new CognitoIdentityServiceProvider({
+  apiVersion: "2016-04-18"
+});
 
 export enum Permissions {
   Admin = "ADMIN",
