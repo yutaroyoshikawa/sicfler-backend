97b7e15a3076128dcb3b400bbefcb229318fd2bc
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2020-02-16 19:31:34 +0900

    credintial修正

--- src/cognito.ts
+++ src/cognito.ts
@@ -2,10 +2,15 @@
 import {
   CognitoIdentityServiceProvider,
   CognitoIdentity,
+  CognitoIdentityCredentials,
   config
 } from "aws-sdk";
 
 config.update({ region: process.env.AWS_REGION! });
+
+config.credentials = new CognitoIdentityCredentials({
+  IdentityPoolId: process.env.COGNITO_IDENTITY_POOL_ID!
+});
 
 export const cognitoAdmin = new CognitoIdentityServiceProvider({
   apiVersion: "2016-04-18"
